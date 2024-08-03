15bab4d4b5a2326084c8c34291734ef65887c898
vanesazdravkova <vanesa.zdravkova@gmail.com>
2024-08-03 22:08:41 +0300

    Homepage and internationalisation

---
+++ src/main/java/bg/softuni/travelProject/web/HomeController.java
@@ -0,0 +1,15 @@
+package bg.softuni.travelProject.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+
+        return "index";
+    }
+}
